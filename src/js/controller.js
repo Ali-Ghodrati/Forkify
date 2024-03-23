44dd34e253050c75127ae37ede24cba2d1988527
Ali Ghodrati <alighodrati1371@gmail.com>
2024-03-23 22:45:22 +0330

    add load recipe from API

--- src/js/controller.js
+++ src/js/controller.js
@@ -10,4 +10,37 @@ const timeout = function (s) {
 
 // https://forkify-api.herokuapp.com/v2
 
-///////////////////////////////////////
+async function loadRecipe(id) {
+  try {
+    const res = await fetch(
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
+    );
+    const data = await res.json();
+
+    const recipe = data.data.recipe;
+
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+
+    console.log(recipe);
+
+    const newRecipe = {
+      id: recipe.id,
+      image: recipe.image_url,
+      source: recipe.source_url,
+      publisher: recipe.publisher,
+      servings: recipe.servings,
+      title: recipe.title,
+      ingredients: recipe.ingredients,
+      cookingTime: recipe.cooking_time,
+    };
+
+    console.log(newRecipe);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+window.addEventListener('hashchange', function () {
+  const id = location.hash.slice(1);
+  loadRecipe(id);
+});
